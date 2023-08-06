30268d029e7a42795b7dda0f182c573752671dc7
Lucas Bastos <lucsbasto@gmail.com>
2023-08-06 17:54:28 -0300

    feat: ensure SignUpController calls AddAccount with correct values

---
+++ src/domain/usecases/add-account.ts
@@ -0,0 +1,11 @@
+import { type AccountModel } from '../models/account'
+
+export interface AddAccount {
+  add: (account: AddAccountModel) => AccountModel
+}
+
+export interface AddAccountModel {
+  name: string
+  email: string
+  password: string
+}
